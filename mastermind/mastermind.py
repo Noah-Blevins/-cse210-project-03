fe7a5a0657075d249bd9657bf670e52af9642d9d
owamamwen1 <90800458+owamamwen1@users.noreply.github.com>
2021-10-02 18:34:07 +0200

    Update mastermind.py
    Code added

--- mastermind/mastermind.py
+++ mastermind/mastermind.py
@@ -1,4 +1,34 @@
 
+import random
+
 
 def get_mastermind():
-    print("Write the code here")
+
+    # function generates number
+    # random within the range.
+    num = random.randrange(1000, 10000)
+    print("???????????????????????????????????")
+    print("Please you need to input integer values")
+    print("Guess the 4 digit number")
+    print("???????????????????????????????????")
+
+    # names of the player
+    playerOne = str(input("Enter a name for player 1: "))
+    playerTwo = str(input("Enter a name for player 2: "))
+
+    # print out the player names
+    print("-------------------------------\n")
+    print(f"Player {playerOne} : ----, ****")
+    print(f"Player {playerTwo} : ----, ****")
+    print("-------------------------------\n")
+
+    # player one
+    print(f"{playerOne}'s turn:")
+    player_a = int(input("What is your guess? "))
+
+    print("-------------------------------\n")
+    # player two
+    print(f"{playerTwo}'s turn:")
+    player_b = int(input("What is your guess? "))
+
+    print("-------------------------------\n")
