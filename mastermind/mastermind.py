8b12ad3fbba8b77171fc254455e3c47e13b88766
Noah Blevins <37628172+Noah-Blevins@users.noreply.github.com>
2021-10-02 23:55:29 -0600

    Update mastermind.py
    update to work

--- mastermind/mastermind.py
+++ mastermind/mastermind.py
@@ -1,16 +1,29 @@
-
 import random
 
 
-def get_mastermind():
+def _create_hint(code, guess):
+    """Generates a hint based on the given code and guess.
+    Args:
+        code (string): The code to compare with.
+        guess (string): The guess that was made.
+    Returns:
+        string: A hint in the form [xxxx]
+    """
+    code = str(code)
+    guess = str(guess)
+    hint = ""
+    for index, letter in enumerate(guess):
+        if code[index] == letter:
+            hint += "x"
+        elif letter in code:
+            hint += "o"
+        else:
+            hint += "*"
+    return hint
 
-    # function generates number
-    # random within the range.
-    num = random.randrange(1000, 10000)
-    print("???????????????????????????????????")
-    print("Please you need to input integer values")
-    print("Guess the 4 digit number")
-    print("???????????????????????????????????")
+
+def get_mastermind():
+    num1 = random.randrange(1000, 10000)
 
     # names of the player
     playerOne = str(input("Enter a name for player 1: "))
@@ -32,12 +45,26 @@ def get_mastermind():
     player_b = int(input("What is your guess? "))
 
     print("-------------------------------\n")
-    
-    # guess condition terminates if true.
-    if (player_a == num):
-        print(f"{playerOne} Won!")
-    elif (player_b == num):
-        print(f"{playerTwo} Won!")
-    else:
-        print("End of the Mastermind!")
-    # loops until one guesses the right answer
+    while True:
+        # player one guesses
+        print(f"{playerOne}'s turn:")
+        player_a = int(input("What is your guess? "))
+        print(_create_hint(num1, player_a))
+
+        print("-------------------------------\n")
+        # player two guesses
+        print(f"{playerTwo}'s turn:")
+        player_b = int(input("What is your guess? "))
+
+        print(_create_hint(num1, player_b))
+
+        print("-------------------------------\n")
+
+        # guess condition terminates if true.
+        if (player_a == num1):
+            print(f"{playerOne} Won!")
+            break
+        elif (player_b == num1):
+            print(f"{playerTwo} Won!")
+            break
+
